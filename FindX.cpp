1b8766aa55fa897606a08cb05ac2d307986dbe03
faizzahmedkhan <akfaiz45@gmail.com>
2025-04-23 02:57:00 +0500

    Board Display

--- FindX.cpp
+++ FindX.cpp
@@ -8,12 +8,7 @@ using namespace std;
 int main()
 {
     int twoD[25][2] = {
-        {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, 
-        {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, 
-        {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, 
-        {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, 
-        {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}
-    };
+        {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 1}, {2, 2}, {2, 3}, {2, 4}, {2, 5}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {3, 5}, {4, 1}, {4, 2}, {4, 3}, {4, 4}, {4, 5}, {5, 1}, {5, 2}, {5, 3}, {5, 4}, {5, 5}};
     int me[] = {1, 1};
     int x[2];
     srand(time(NULL));
@@ -22,6 +17,7 @@ int main()
 
     while (i < 10 && (me[0] != x[0] || me[1] != x[1]))
     {
+        
         cout << "Enter your move" << endl;
         int move = getch(); // Get the first character
 
@@ -78,16 +74,30 @@ int main()
                 cout << "Incorrect move" << endl;
                 i--;
             }
-            cout<<"You have "<<10-(i+1)<<" chances left"<<endl;
+            cout << "You have " << 10 - (i + 1) << " chances left" << endl;
         }
         else
         {
             cout << "Invalid input. Use arrow keys!" << endl;
             i--;
         }
-
-        cout << "You are at " << me[0] << "," << me[1] << endl;
+        
+        // cout << "You are at " << me[0] << "," << me[1] << endl;
         i++;
+        for (int k = 1; k < 6; k++)
+        {
+            for (int l = 1; l < 6; l++)
+            {
+                if (me[0]==k && me[1]==l)
+                {
+                    cout<<"X  ";
+                    l++;
+                }
+                
+                cout << "__ ";
+            }
+            cout << "" << endl;
+        }
     }
 
     if ((me[0] == x[0] && me[1] == x[1]))
@@ -96,8 +106,23 @@ int main()
     }
     else
     {
-        cout << "You lose" << endl;
-        cout << x[0] <<" " << x[1] << endl;
+        cout << "You lose " << endl;
+        cout << "X was here" << endl;
+        for (int k = 1; k < 6; k++)
+        {
+            for (int l = 1; l < 6; l++)
+            {
+                if (x[0]==k && x[1]==l)
+                {
+                    cout<<"X  ";
+                    l++;
+                }
+                
+                cout << "__ ";
+            }
+            cout << "" << endl;
+        }
+        // cout << x[0] << " " << x[1] << endl;
     }
 
     return 0;
